873b77e150a4f47cab4554d7b19126b7604b9060
admin <admin@YD01-05>
2021-02-22 09:58:10 +0900

    .

---
+++ javaProject/src/Operator/IncreaseDecreaseOpweatorExample.java
@@ -0,0 +1,19 @@
+package Operator;
+
+public class IncreaseDecreaseOpweatorExample {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		System.out.println(x++);
+		System.out.println(++x);
+		System.out.println(x); //12
+		System.out.println("-----------------");
+		z = ++x + ++y; //x: 13, y: 11
+		System.out.println("z: " + z + ", x: " + x + ", y: " +y);
+		
+	}
+
+}
